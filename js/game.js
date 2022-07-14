baf80d428e918ee8493905e2d3f2271b8d1c1486
mickey-40 <mickeyarnold53@yahoo.com>
2022-07-13 21:46:59 -0500

    working on stopping the intervals when button is push

--- js/game.js
+++ js/game.js
@@ -1,10 +1,19 @@
 const nameScore = document.querySelector('.name')
 const questionElement = document.querySelector('.question')
 const choices = Array.from(document.querySelectorAll('.choice-text'))
+const scoreText = document.querySelector('.currentScore')
 let randomQuestion
-let currentQuestionIndex
+let currentQuestion
 let askedQuestion = []
 let questionNum = 1
+let acceptingAnswers = true
+let timerInterval
+let scoreInterval
+// Set timer to count down from 30
+let startingTimer = 30;
+//Set maxScore to 1000
+let maxScore = 1000;
+
 
 
 
@@ -22,37 +31,66 @@ document.querySelector('.submit').addEventListener('click', function(){
   document.querySelector('.gameContainer').classList.remove('hidden')
   player1.label = document.querySelector('#name').value
   document.querySelector('.name').innerText = player1.label + ' ' + player1.score
-  setInterval(timer, 1000)
-  setInterval(scoreCounter, 1000)
+  // setInterval(timer, 1000)
+  // setInterval(scoreCounter, 1000)
   getquestion()
   
 })
 //A function that puts a random question with the answer choices
-getquestion = () => {
+let getquestion = () => {
+   
+  timerInterval = setInterval(timer, 1000)
+  scoreInterval = setInterval(scoreCounter, 1000)
   
   randomQuestion = Math.floor(Math.random() * questions.length)
-  currentQuestionIndex = questions[randomQuestion]
-  askedQuestion.push(currentQuestionIndex)
-  questionElement.innerText = `${questionNum}. ${currentQuestionIndex.question}`
+  currentQuestion = questions[randomQuestion]
+  askedQuestion.push(currentQuestion)
+  questionElement.innerText = `${questionNum}. ${currentQuestion.question}`
   //enters the answer choices
   choices.forEach(choice => {
     const number = choice.dataset['number']
-    choice.innerText = currentQuestionIndex['choice' + number]
+    choice.innerText = currentQuestion['choice' + number]
   })
   //takes current question out of the array
-  questions.splice(currentQuestionIndex,1)
+  questions.splice(currentQuestion,1)
+  acceptingAnswers = true
   questionNum++
-  
-
 
 }
+choices.forEach(choice => {
+  choice.addEventListener('click', e => {
+    if (!acceptingAnswers) return
 
+    acceptingAnswers = false
+    const selectedChoice = e.target
+    const selectedAnswer = selectedChoice.dataset['number']
 
+    let classToApply = selectedAnswer == questions.answer ? 'correct' : 'incorrect'
 
-// Set timer to count down from 30
-let startingTimer = 30;
-//Set maxScore to 1000
-let maxScore = 1000;
+    if (classToApply === 'correct') {
+      incrementScore(maxScore)
+    }
+
+    selectedChoice.parentElement.classList.add(classToApply)
+
+    setTimeout(() => {
+      selectedChoice.parentElement.classList.remove(classToApply)
+      getquestion()
+      clearIntervals()
+
+    }, 1000)
+  })
+})
+
+let clearIntervals = () => {
+    clearInterval(timerInterval)
+    clearInterval(scoreInterval)
+}
+
+incrementScore = (num) => {
+  player1.score += num
+  scoreText.innerText = `${player1.score}`
+}
 
 // Update the count down every 1 second
 timer = () => {
@@ -114,132 +152,132 @@ const questions = [
     answer: 4,
 
   },
-  {
-    question:'If x + 4 = b, what is the value of x?',
-    choice1:'4b',
-    choice2:'-4b',
-    choice3:'b + 4',
-    choice4:'b - 4',
-    answer: 4,
+  // {
+  //   question:'If x + 4 = b, what is the value of x?',
+  //   choice1:'4b',
+  //   choice2:'-4b',
+  //   choice3:'b + 4',
+  //   choice4:'b - 4',
+  //   answer: 4,
 
-  },
-  {
-    question:'An equation must have two equal what?',
-    choice1:'Examples',
-    choice2:'Expressions',
-    choice3:'Extensions',
-    choice4:'Extractions',
-    answer: 2,
+  // },
+  // {
+  //   question:'An equation must have two equal what?',
+  //   choice1:'Examples',
+  //   choice2:'Expressions',
+  //   choice3:'Extensions',
+  //   choice4:'Extractions',
+  //   answer: 2,
 
-  },
-  {
-    question:'If you are given an equation, what will you have done to it when you have found the answers?',
-    choice1:'Soaked it',
-    choice2:'Solved it',
-    choice3:'Squared it',
-    choice4:'Stuffed it',
-    answer: 2,
+  // },
+  // {
+  //   question:'If you are given an equation, what will you have done to it when you have found the answers?',
+  //   choice1:'Soaked it',
+  //   choice2:'Solved it',
+  //   choice3:'Squared it',
+  //   choice4:'Stuffed it',
+  //   answer: 2,
 
-  },
-  {
-    question:'Which of these is not an equation?',
-    choice1:'6/3 = 2',
-    choice2:'65 + 35 = 100',
-    choice3:'5 - 6 = 8 - 9',
-    choice4:'2 x 3 = 7 - 2',
-    answer: 4,
+  // },
+  // {
+  //   question:'Which of these is not an equation?',
+  //   choice1:'6/3 = 2',
+  //   choice2:'65 + 35 = 100',
+  //   choice3:'5 - 6 = 8 - 9',
+  //   choice4:'2 x 3 = 7 - 2',
+  //   answer: 4,
 
-  },
-  {
-    question:'< is the sign that means what?',
-    choice1:'Less than',
-    choice2:'Greater than',
-    choice3:'Less than or equal to',
-    choice4:'Greater than or equal to',
-    answer: 1,
+  // },
+  // {
+  //   question:'< is the sign that means what?',
+  //   choice1:'Less than',
+  //   choice2:'Greater than',
+  //   choice3:'Less than or equal to',
+  //   choice4:'Greater than or equal to',
+  //   answer: 1,
 
-  },
-  {
-    question:'If x > 3, what is the smallest integer that x can be?',
-    choice1:'2',
-    choice2:'3',
-    choice3:'4',
-    choice4:'5',
-    answer: 3,
+  // },
+  // {
+  //   question:'If x > 3, what is the smallest integer that x can be?',
+  //   choice1:'2',
+  //   choice2:'3',
+  //   choice3:'4',
+  //   choice4:'5',
+  //   answer: 3,
 
-  },
-  {
-    question:'If 4x + 3 < 15, which of the following expressions are incorrect?',
-    choice1:'4x < 15 - 3',
-    choice2:'4x < 12',
-    choice3:'x < 3',
-    choice4:'x > 3',
-    answer: 4,
+  // },
+  // {
+  //   question:'If 4x + 3 < 15, which of the following expressions are incorrect?',
+  //   choice1:'4x < 15 - 3',
+  //   choice2:'4x < 12',
+  //   choice3:'x < 3',
+  //   choice4:'x > 3',
+  //   answer: 4,
 
-  },
-  {
-    question:' A small submarine started its dive at sea level and descended 30 feet per minute. Which integer represents the submarine’s depth after seven minutes?',
-    choice1:'-210 feet',
-    choice2:'-23 feet',
-    choice3:'37 feet',
-    choice4:'210 feet',
-    answer: 1,
+  // },
+  // {
+  //   question:' A small submarine started its dive at sea level and descended 30 feet per minute. Which integer represents the submarine’s depth after seven minutes?',
+  //   choice1:'-210 feet',
+  //   choice2:'-23 feet',
+  //   choice3:'37 feet',
+  //   choice4:'210 feet',
+  //   answer: 1,
 
-  },
-  {
-    question:'Which rational number is the greatest?',
-    choice1:'0.55',
-    choice2:'0.6',
-    choice3:'9/20',
-    choice4:'1/2',
-    answer: 2,
+  // },
+  // {
+  //   question:'Which rational number is the greatest?',
+  //   choice1:'0.55',
+  //   choice2:'0.6',
+  //   choice3:'9/20',
+  //   choice4:'1/2',
+  //   answer: 2,
 
-  },
-  {
-    question:'What is the value of the expression 8x – 10 when x = 5?',
-    choice1:'3',
-    choice2:'4',
-    choice3:'30',
-    choice4:'50',
-    answer: 3,
+  // },
+  // {
+  //   question:'What is the value of the expression 8x – 10 when x = 5?',
+  //   choice1:'3',
+  //   choice2:'4',
+  //   choice3:'30',
+  //   choice4:'50',
+  //   answer: 3,
 
-  },
-  {
-    question:'The attendance at three concerts was 876, 647, and 856. Which expression shows how to estimate the total attendance at the concerts?',
-    choice1:'800 + 600 + 800',
-    choice2:'900 + 700 + 900',
-    choice3:'900 + 600 + 900',
-    choice4:' 1,000 + 700 + 900',
-    answer: 3,
+  // },
+  // {
+  //   question:'The attendance at three concerts was 876, 647, and 856. Which expression shows how to estimate the total attendance at the concerts?',
+  //   choice1:'800 + 600 + 800',
+  //   choice2:'900 + 700 + 900',
+  //   choice3:'900 + 600 + 900',
+  //   choice4:' 1,000 + 700 + 900',
+  //   answer: 3,
 
-  },
-  {
-    question:'What is 35% of 80?',
-    choice1:'28',
-    choice2:'45',
-    choice3:'115',
-    choice4:'2800',
-    answer: 1,
+  // },
+  // {
+  //   question:'What is 35% of 80?',
+  //   choice1:'28',
+  //   choice2:'45',
+  //   choice3:'115',
+  //   choice4:'2800',
+  //   answer: 1,
 
-  },
-  {
-    question:'Which expression represents 4 less than twice a number, n ?',
-    choice1:'4 - n',
-    choice2:'n - 4',
-    choice3:'4 - 2n',
-    choice4:'2n - 4',
-    answer: 4,
+  // },
+  // {
+  //   question:'Which expression represents 4 less than twice a number, n ?',
+  //   choice1:'4 - n',
+  //   choice2:'n - 4',
+  //   choice3:'4 - 2n',
+  //   choice4:'2n - 4',
+  //   answer: 4,
 
-  },
-  {
-    question:'Johnny jogged around the track 16 times. Each lap is 400 meters. Which process could determine the total distance he jogged?',
-    choice1:'add 16 and 400',
-    choice2:'divide 400 by 16',
-    choice3:'multiply 400 by 16',
-    choice4:'subtract 16 from 400',
-    answer: 3,
+  // },
+  // {
+  //   question:'Johnny jogged around the track 16 times. Each lap is 400 meters. Which process could determine the total distance he jogged?',
+  //   choice1:'add 16 and 400',
+  //   choice2:'divide 400 by 16',
+  //   choice3:'multiply 400 by 16',
+  //   choice4:'subtract 16 from 400',
+  //   answer: 3,
 
-  }
+  // }
 ]
 
 
