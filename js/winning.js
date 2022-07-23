978467adfe1f4c351b4ed68087bb07e1fdfe1add
mickey-40 <mickeyarnold53@yahoo.com>
2022-07-23 09:38:41 -0500

    refactoring code for winning.js file

--- js/winning.js
+++ js/winning.js
@@ -35,4 +35,18 @@ localStorage.setItem('highScores', JSON.stringify(highScores))
 console.log(highScores)
 
 
+//targets where the high scores are going to go
+const highScoreList = document.querySelector('#highScoreList')
+//get the high score array from localStorage
+const highScore = JSON.parse(localStorage.getItem('highScores')) || []
 
+
+console.log(highScore.length)
+
+
+//a function that goes through the array in localStorage and list it in the unordered list
+
+highScoreList.innerHTML = highScore.map(playerInfo => {
+  return `<li>${playerInfo.name}-${playerInfo.score}</li>`
+}).join('')
+  
